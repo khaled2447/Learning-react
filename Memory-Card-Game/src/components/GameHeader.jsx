7ed355faec6d3a1e5e63ff0a980a4696cd0b1248
Khaled <elshitanyk@gmail.com>
2026-03-19 17:53:17 +0200

    Added score and move counters to header

--- Memory-Card-Game/src/components/GameHeader.jsx
+++ Memory-Card-Game/src/components/GameHeader.jsx
@@ -3,8 +3,14 @@ export const GameHeader = () => {
     return <div className="game-header">
         <h1>Memory Card Game!!</h1>
         <div className="stats">
-            <div className="stat-item">score 0</div>
-            <div className="stat-item">moves 0</div>
+            <div className="stat-item">
+                <span className="stat-label">score</span> 
+                <span className="stat-value">0</span>
+            </div>
+            <div className="stat-item">
+                <span className="stat-label">moves</span> 
+                <span className="stat-value">0</span>
+            </div>
         </div>
     </div>
 }
