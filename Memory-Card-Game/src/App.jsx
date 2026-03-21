232aaf9c41ccc39d09c27937f45e71606e4b2df3
Khaled <elshitanyk@gmail.com>
2026-03-21 10:56:33 +0200

    displayed cards

--- Memory-Card-Game/src/App.jsx
+++ Memory-Card-Game/src/App.jsx
@@ -1,7 +1,19 @@
 import { GameHeader } from "./components/GameHeader"
+import { Card } from "./components/Card"
+
+const cardValues = ["西", "北", "東", "南", "赤", "青", "緑", "白", "西", "北", "東", "南", "赤", "青", "緑", "白"]
 
 function App() {
-  return <div className="app"><GameHeader /></div>
+  return <div className="app">
+    <GameHeader score={2} moves={3} />
+
+    <div className="cards-grid">
+      {cardValues.map((card) => (        
+        <Card card={card} />
+      ))}
+    </div>
+
+  </div>
 }
 
 export default App
--- Memory-Card-Game/src/components/GameHeader.jsx
+++ Memory-Card-Game/src/components/GameHeader.jsx
@@ -1,15 +1,15 @@
 
-export const GameHeader = () => {
+export const GameHeader = ({score, moves}) => {
     return <div className="game-header">
         <h1>Memory Card Game!!</h1>
         <div className="stats">
             <div className="stat-item">
                 <span className="stat-label">score</span> 
-                <span className="stat-value">0</span>
+                <span className="stat-value">{score}</span>
             </div>
             <div className="stat-item">
                 <span className="stat-label">moves</span> 
-                <span className="stat-value">0</span>
+                <span className="stat-value">{moves}</span>
             </div>
         </div>
     </div>
