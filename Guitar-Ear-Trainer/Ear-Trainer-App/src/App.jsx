5e54048c05469ba479f4a3c403a3b5edd14861cf
Khaled <elshitanyk@gmail.com>
2026-03-26 18:59:06 +0200

    can someone download tailwind into my brain

--- Guitar-Ear-Trainer/Ear-Trainer-App/src/App.jsx
+++ Guitar-Ear-Trainer/Ear-Trainer-App/src/App.jsx
@@ -1,9 +1,13 @@
-//import './App.css'
+import {notes, scales, guitarNotes} from "./data/notes"
 
 function App() {
 
   return (
-    <p>{notes}</p>
+    <div className= "bg-white dark:bg-gray-800 px-6 py-8 ring shadow-xl ring-gray-900/5 h-screen">
+      <div className="grid grid-cols-12 gap-4">
+        {guitarNotes.map((note) => {return <span className="text-amber-400" key={note}>{note}</span>})}
+      </div>
+    </div>
   )
 }
 
